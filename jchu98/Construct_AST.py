e73998482e0b7a6beb1777a1a0b142730e16f705
Juheon Chu <chuj@dickinson.edu>
2023-10-24 14:34:11 -0400

    Grammar defined upto operators for CBASIC.
    Co-authored-by: shaikhibrahim2000 <ahnafrahman305@gmail.com>

--- jchu98/Construct_AST.py
+++ jchu98/Construct_AST.py
@@ -2,6 +2,40 @@ from antlr4 import *
 from ExprLexer import ExprLexer
 from ExprParser import ExprParser
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+def parse(tokens):
+    stack = []
+    for token in tokens:
+        if token == '(':
+            stack.append(Node(None))
+        elif token == ')':
+            child = stack.pop()
+            if stack:
+                stack[-1].children.append(child)
+            else:
+                return child
+        else:
+            stack[-1].children.append(Node(token))
+    return stack[0]
+
+def tokenize(input_string):
+    return input_string.replace('(', ' ( ').replace(')', ' ) ').split()
+
+def traverse(node, line=1):
+    output = []
+    output.append((node.value, line))
+    for child in node.children:
+        line += 1
+        child_output, line = traverse(child, line)
+        output.extend(child_output)
+    return output, line
+
+
+
 # Open and read from the file 'bas.bas'
 with open('TEST.BAS', 'r') as file:
     input_stream = InputStream(file.read())
@@ -14,5 +48,17 @@ parser = ExprParser(token_stream)
 
 tree = parser.program()  # Use the starting rule (assuming it's 'program')
 
-print(tree.toStringTree(recog=parser))
+
+# Construct the tokenized Abstract Syntax Tree 
+tokenized_string = tree.toStringTree(recog=parser) 
+
+tokens = tokenize(tokenized_string)
+ast = parse(tokens)
+output, _ = traverse(ast)
+
+
+for node, line in output[1:]:  # skip the root
+    print(f"{node},{line}")
+
+
 
